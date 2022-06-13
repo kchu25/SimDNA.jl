a3a87a9a545dff8ef806108d163b1fa02d1d5d2a
kchu25 <skchu@wustl.edu>
2022-06-13 14:27:49 -0500

    version change and type export

--- src/SimDNA.jl
+++ src/SimDNA.jl
@@ -6,7 +6,9 @@ export Sim_DNA,
        single_block_motif,
        gapped_k_block_motif,
        mixture_k_block_motif,
-       mixture_gapped_k_block_motif
+       mixture_gapped_k_block_motif,
+       motif_type,
+       sim_dna_str_w_motif
 
 
 # println("ho")
